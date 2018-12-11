24aae0d01db25cc02973cc08980899d259486d5a
Laboratorios <Laboratorios@R20>
2018-12-10 20:57:29 -0600

    a

--- PAW.ExamenFinal.Wcf/Logica/Accion/Metodo_de_Accion.cs
+++ PAW.ExamenFinal.Wcf/Logica/Accion/Metodo_de_Accion.cs
@@ -7,5 +7,11 @@ namespace PAW.ExamenFinal.Wcf.Logica.Accion
 {
     public class Metodo_de_Accion
     {
+        public ModelDB.Cliente IdentificarCliente(int idCliente)
+        {
+            var elRepositorio = new Logica.Metodo_De_Repositorio.Clientes();
+            var elResultado = elRepositorio.IdentificarCliente(idCliente);
+            return elResultado;
+        }
     }
 }
