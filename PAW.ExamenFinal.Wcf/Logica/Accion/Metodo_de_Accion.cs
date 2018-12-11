35891f4e70a164579193de8f9cb2fee87db9aedb
Laboratorios <Laboratorios@R20>
2018-12-10 20:39:08 -0600

    Creacion metodo accion

---
+++ PAW.ExamenFinal.Wcf/Logica/Accion/Metodo_de_Accion.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PAW.ExamenFinal.Wcf.Logica.Accion
+{
+    public class Metodo_de_Accion
+    {
+    }
+}
